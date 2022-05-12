eebab7f50aeb1b59578d5846d49add44d722177a
bobozhezhe <bo.huang.2017@gmail.com>
2022-05-12 21:40:06 +0800

    Solution of No3

--- arrays/SortedSquaredArray.java
+++ arrays/SortedSquaredArray.java
@@ -1,1 +1,16 @@
+package Leetcode_Bourne.arrays;
 
+import java.util.*;
+
+public class SortedSquaredArray {
+    // O(nlogn) time | O(n) space - where n is the length of the input array
+    public int[] sortedSquaredArray(int[] array) {
+        int[] sortedSquares = new int[array.length];
+        for (int idx = 0; idx < array.length; idx++) {
+            int value = array[idx];
+            sortedSquares[idx] = value * value;
+        }
+        Arrays.sort(sortedSquares);
+        return sortedSquares;
+    }
+}
