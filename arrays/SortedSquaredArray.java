1fdd77e0ace5bc331f4e054942d886eb472405fd
bobozhezhe <bo.huang.2017@gmail.com>
2022-05-13 08:16:51 +0800

    Solution2 of No3

--- arrays/SortedSquaredArray.java
+++ arrays/SortedSquaredArray.java
@@ -3,8 +3,28 @@ package Leetcode_Bourne.arrays;
 import java.util.*;
 
 public class SortedSquaredArray {
+
+    public static void main(String[] args) {
+        int[] input_array = new int[]{1, 2, 3, 5, 6, 8, 9};
+        int[] res_array = new int[]{1, 4, 9, 25, 36, 64, 81};
+
+        int[] res = sortedSquaredArray1(input_array);
+        if(Arrays.equals(res, res_array)){
+            System.out.println("1: True!");
+        }else {
+            System.out.println("1: False!");
+        }
+
+        res = sortedSquaredArray2(input_array);
+        if(Arrays.equals(res, res_array)){
+            System.out.println("2: True!");
+        }else {
+            System.out.println("2: False!");
+        }
+    }
+
     // O(nlogn) time | O(n) space - where n is the length of the input array
-    public int[] sortedSquaredArray(int[] array) {
+    public static int[] sortedSquaredArray1(int[] array) {
         int[] sortedSquares = new int[array.length];
         for (int idx = 0; idx < array.length; idx++) {
             int value = array[idx];
@@ -12,5 +32,21 @@ public class SortedSquaredArray {
         }
         Arrays.sort(sortedSquares);
         return sortedSquares;
+    }
+
+    // O(n) time | O(n) space - where n is the length of the input array
+    public static int[] sortedSquaredArray2(int[] array){
+        int head = 0, end = array.length -1;
+        int[] squaredArray = new int[array.length];
+        for(int idx =0; idx < array.length; idx++){
+            if(Math.abs(array[head]) < Math.abs(array[end])) {
+                squaredArray[idx] = array[head] * array[head];
+                head++;
+            }else {
+                squaredArray[idx] = array[end] * array[end];
+                end++;
+            }
+        }
+        return squaredArray;
     }
 }
--- arrays/SortedSquaredArray.md
+++ arrays/SortedSquaredArray.md
@@ -1,3 +1,20 @@
 # Sorted Squared Array
 Write a function that takes in a non-empty array of integers that are sorted in ascending order 
 and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
+
+输入：非空升序整数数组（可能为负）
+
+输出：原数组的平方，升序数组
+
+# Solution 1
+先平方，再快排
+
+Time: O(nlogn), Space: O(n).
+
+# Solution 2
+遍历一遍，完成绝对值比较再平方的过程。
+
+因为数组的两端只能看到绝对值最大的数，所以，只能从大到小逆向来完成排序。
+
+# JAVA
+Arrays.sort();
