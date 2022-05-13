ca5e37a17478469cfcab57633b6bf20c1174bbb6
bobozhezhe <bo.huang.2017@gmail.com>
2022-05-13 08:49:27 +0800

    Fix bugs in No3.

--- arrays/SortedSquaredArray.java
+++ arrays/SortedSquaredArray.java
@@ -5,22 +5,30 @@ import java.util.*;
 public class SortedSquaredArray {
 
     public static void main(String[] args) {
-        int[] input_array = new int[]{1, 2, 3, 5, 6, 8, 9};
-        int[] res_array = new int[]{1, 4, 9, 25, 36, 64, 81};
+        int[][] input_array = new int[][]{{1, 2, 3, 5, 6, 8, 9}, {-2, -1}};
+        int[][] res_array = new int[input_array.length][];
 
-        int[] res = sortedSquaredArray1(input_array);
-        if(Arrays.equals(res, res_array)){
-            System.out.println("1: True!");
-        }else {
-            System.out.println("1: False!");
-        }
+        for (int i =0; i < input_array.length; i++) {
+            res_array[i] = sortedSquaredArray1(input_array[i]);
 
-        res = sortedSquaredArray2(input_array);
-        if(Arrays.equals(res, res_array)){
-            System.out.println("2: True!");
-        }else {
-            System.out.println("2: False!");
+            int[] res = sortedSquaredArray2(input_array[i]);
+            if (Arrays.equals(res, res_array[i])) {
+                System.out.println(i + ": True!");
+            } else {
+                System.out.println(i + ": False!");
+                for(int arr : res){
+                    System.out.print(arr + ", ");
+                }
+                System.out.println();
+            }
         }
+//        int[] res = sortedSquaredArray1(input_array);
+//        if(Arrays.equals(res, res_array)){
+//            System.out.println("1: True!");
+//        }else {
+//            System.out.println("1: False!");
+//        }
+
     }
 
     // O(nlogn) time | O(n) space - where n is the length of the input array
@@ -38,14 +46,10 @@ public class SortedSquaredArray {
     public static int[] sortedSquaredArray2(int[] array){
         int head = 0, end = array.length -1;
         int[] squaredArray = new int[array.length];
-        for(int idx =0; idx < array.length; idx++){
-            if(Math.abs(array[head]) < Math.abs(array[end])) {
-                squaredArray[idx] = array[head] * array[head];
-                head++;
-            }else {
-                squaredArray[idx] = array[end] * array[end];
-                end++;
-            }
+        for(int idx = array.length-1; idx >= 0; idx--) {
+            squaredArray[idx] = (Math.abs(array[head]) > Math.abs(array[end])) ?
+                array[head] * array[head++] :
+                array[end] * array[end--];
         }
         return squaredArray;
     }
